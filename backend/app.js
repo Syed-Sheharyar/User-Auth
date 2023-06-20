df89e60d9e0d1020718a228df46acbfbc2f2e41a
Syed Sheharyar <syedsheharyartariq146@gmail.com>
2023-06-20 21:18:31 +0500

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieparser())
-app.use(cors())
+app.use(cors({ origin: "*" }))
 
 // Serving static files
 app.use(express.static(__dirname + "/public"))
