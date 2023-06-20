6fab523d31cdc0cd885799dc25ea32d42d9d2800
Syed Sheharyar <syedsheharyartariq146@gmail.com>
2023-06-20 20:01:12 +0500

    export app

--- backend/server.js
+++ backend/server.js
@@ -3,28 +3,26 @@ const dotenv = require("dotenv")
 const connectDb = require("./config/database")
 
 // Handling Uncaught Exception
-process.on("uncaughtException", (err)=>{
+process.on("uncaughtException", (err) => {
     console.log(`Error ${err.message}`);
     console.log('Server shutdown due to Uncaught Exception');
     process.exit(1)
 })
 
 // dotenv  config
-dotenv.config({path: "backend/config/.env.local"})
+dotenv.config({ path: "backend/config/.env.local" })
 
 // Connecting to Database
 connectDb()
 
-const server = app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running at http://localhost:${process.env.PORT}`);
-})
+module.exports = app
 
 // Unhandled Promise Rejection
-process.on("unhandledRejection", (err)=>{
+process.on("unhandledRejection", (err) => {
     console.log(`Error ${err.message}`);
     console.log('Server shutdown due to Unhandled Promise Rejection');
 
-    server.close(()=>{
-       process.exit(1)
+    server.close(() => {
+        process.exit(1)
     })
 })
